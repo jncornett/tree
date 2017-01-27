b8f20f52101e2419fb0f8e65a839212cbce598a7
Joel Cornett <jcornett@i-a-i.com>
2017-01-26 20:00:14 -0500

    simplify dir and add NodeList

--- dir.go
+++ dir.go
@@ -1,20 +1,14 @@
 package tree
 
 type Dir struct {
-	Name     string
-	Contents []Node
+	Name    string
+	Content Node
 }
 
 func (d Dir) Eval(c Context) (err error) {
 	if c, err = c.Enter(d.Name); err != nil {
 		return
 	}
-	for _, node := range d.Contents {
-		if err = node.Eval(c); err != nil {
-			break
-		}
-	}
+	err = d.Content.Eval(c)
 	return
 }
-
-var _ Node = &Dir{}
--- node.go
+++ node.go
@@ -7,3 +7,14 @@ type Node interface {
 type NodeFunc func(Context) error
 
 func (f NodeFunc) Eval(c Context) error { return f(c) }
+
+type NodeList []Node
+
+func (l NodeList) Eval(c Context) (err error) {
+	for _, node := range l {
+		if err = node.Eval(c); err != nil {
+			return
+		}
+	}
+	return
+}
